4d975c77db580c1c600743220b35b5825eb2e8c0
Habibur-Rahman0927 <habibur.rahaman.0927@gmail.com>
2022-04-25 23:17:15 +0600

    09_vue_project_Monster show game over screen

--- 09_vue_project_Monster/app.js
+++ 09_vue_project_Monster/app.js
@@ -7,6 +7,8 @@ const app = Vue.createApp({
     return {
       playerHeath: 100,
       monsterHealth: 100,
+      currentRound: 0,
+      winner: null,
     };
   },
   computed: {
@@ -16,9 +18,33 @@ const app = Vue.createApp({
     playerBarStyles() {
       return { width: this.playerHeath + "%" };
     },
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
+    },
+  },
+  watch: {
+    playerHeath(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        // A draw
+        this.winner = "draw";
+      } else if (value <= 0) {
+        // lost
+        this.winner = "Monster";
+      }
+    },
+    monsterHealth(value) {
+      if (value <= 0 && this.playerHeath <= 0) {
+        // A draw
+        this.winner = "draw";
+      } else if (value <= 0) {
+        // lost
+        this.winner = "player";
+      }
+    },
   },
   methods: {
     attackMonster() {
+      this.currentRound++;
       const attackValue = getRandomValue(12, 5);
       this.monsterHealth -= attackValue;
       this.attackPlayer();
@@ -26,6 +52,21 @@ const app = Vue.createApp({
     attackPlayer() {
       const attackValue = getRandomValue(15, 8);
       this.playerHeath -= attackValue;
+    },
+    specialAttackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomValue(10, 25);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
+    healPlayer() {
+      const healValue = getRandomValue(8, 20);
+      if (this.playerHeath + healValue > 100) {
+        this.playerHeath = 100;
+      } else {
+        this.playerHeath += healValue;
+      }
+      this.attackPlayer();
     },
   },
 });
