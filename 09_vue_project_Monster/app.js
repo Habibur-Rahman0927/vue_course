9d6d8b32cb2f2af659b621f50288c6c846a10f1c
Habibur-Rahman0927 <habibur.rahaman.0927@gmail.com>
2022-04-25 22:25:16 +0600

    09_vue_project_Monster simple attck done

---
+++ 09_vue_project_Monster/app.js
@@ -0,0 +1,33 @@
+function getRandomValue(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const app = Vue.createApp({
+  data() {
+    return {
+      playerHeath: 100,
+      monsterHealth: 100,
+    };
+  },
+  computed: {
+    monsterBarStyles() {
+      return { width: this.monsterHealth + "%" };
+    },
+    playerBarStyles() {
+      return { width: this.playerHeath + "%" };
+    },
+  },
+  methods: {
+    attackMonster() {
+      const attackValue = getRandomValue(12, 5);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      const attackValue = getRandomValue(15, 8);
+      this.playerHeath -= attackValue;
+    },
+  },
+});
+
+app.mount("#game");
