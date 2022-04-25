aa94b4c535f57b48ed240126328ec60921eac414
Habibur-Rahman0927 <habibur.rahaman.0927@gmail.com>
2022-04-26 00:29:16 +0600

    09_vue_project_Monster project Done

--- 09_vue_project_Monster/app.js
+++ 09_vue_project_Monster/app.js
@@ -9,13 +9,20 @@ const app = Vue.createApp({
       monsterHealth: 100,
       currentRound: 0,
       winner: null,
+      logMessage: [],
     };
   },
   computed: {
     monsterBarStyles() {
+      if (this.monsterHealth < 0) {
+        return { width: "0%" };
+      }
       return { width: this.monsterHealth + "%" };
     },
     playerBarStyles() {
+      if (this.playerHeath < 0) {
+        return { width: "0%" };
+      }
       return { width: this.playerHeath + "%" };
     },
     mayUseSpecialAttack() {
@@ -29,7 +36,7 @@ const app = Vue.createApp({
         this.winner = "draw";
       } else if (value <= 0) {
         // lost
-        this.winner = "Monster";
+        this.winner = "monster";
       }
     },
     monsterHealth(value) {
@@ -47,16 +54,19 @@ const app = Vue.createApp({
       this.currentRound++;
       const attackValue = getRandomValue(12, 5);
       this.monsterHealth -= attackValue;
+      this.addLogMessage("player", "attack", attackValue);
       this.attackPlayer();
     },
     attackPlayer() {
       const attackValue = getRandomValue(15, 8);
+      this.addLogMessage("monster", "attack", attackValue);
       this.playerHeath -= attackValue;
     },
     specialAttackMonster() {
       this.currentRound++;
       const attackValue = getRandomValue(10, 25);
       this.monsterHealth -= attackValue;
+      this.addLogMessage("player", "special-attack", attackValue);
       this.attackPlayer();
     },
     healPlayer() {
@@ -66,7 +76,25 @@ const app = Vue.createApp({
       } else {
         this.playerHeath += healValue;
       }
+      this.addLogMessage("player", "heal", healValue);
       this.attackPlayer();
+    },
+    startGame() {
+      this.playerHeath = 100;
+      this.monsterHealth = 100;
+      this.currentRound = 0;
+      this.winner = null;
+      this.logMessage = [];
+    },
+    surrender() {
+      this.winner = "monster";
+    },
+    addLogMessage(who, what, value) {
+      this.logMessage.unshift({
+        actionBy: who,
+        actionType: what,
+        actionValue: value,
+      });
     },
   },
 });
