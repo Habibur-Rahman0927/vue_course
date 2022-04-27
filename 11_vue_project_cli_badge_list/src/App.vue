3f785d71697ea1da587dda7234cc20151166a1af
Habibur-Rahman0927 <habibur.rahaman.0927@gmail.com>
2022-04-27 23:23:04 +0600

    11_vue_project_cli_badge_list dynamic component rendering and keep alive

--- 11_vue_project_cli_badge_list/src/App.vue
+++ 11_vue_project_cli_badge_list/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <TheHeader />
-    <BadgeList />
+    <!-- <BadgeList />
     <UserInfo
       :full-name="activeUser.name"
       :info-text="activeUser.description"
@@ -12,31 +12,47 @@
         {{ slotProps.goal }}
       </h1>
       <p>{{ slotProps.another }}</p>
-    </CourseGoals>
+    </CourseGoals> -->
+    <button @click="setSeletedComponent('active-goals')">Active Goal</button>
+    <button @click="setSeletedComponent('manage-goals')">Manage Goal</button>
+    <!-- <ActiveGoals v-if="selectedComponent === 'active-goals'"></ActiveGoals>
+    <ManageGoals v-if="selectedComponent === 'manage-goals'"></ManageGoals> -->
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader.vue";
-import BadgeList from "./components/BadgeList.vue";
-import UserInfo from "./components/UserInfo.vue";
-import CourseGoals from "./components/CourseGoals.vue";
-
+// import BadgeList from "./components/BadgeList.vue";
+// import UserInfo from "./components/UserInfo.vue";
+// import CourseGoals from "./components/CourseGoals.vue";
+import ActiveGoals from "./components/ActiveGoals.vue";
+import ManageGoals from "./components/ManageGoals.vue";
 export default {
   components: {
     TheHeader,
-    BadgeList,
-    UserInfo,
-    CourseGoals,
+    // BadgeList,
+    // UserInfo,
+    // CourseGoals,
+    ActiveGoals,
+    ManageGoals,
   },
   data() {
     return {
+      selectedComponent: "active-goals",
       activeUser: {
         name: "Maximilian Schwarzmüller",
         description: "Site owner and admin",
         role: "admin",
       },
     };
+  },
+  methods: {
+    setSeletedComponent(cmp) {
+      this.selectedComponent = cmp;
+    },
   },
 };
 </script>
---
+++ 11_vue_project_cli_badge_list/src/components/ManageGoals.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h2>Manage Goals</h2>
+    <input type="text" name="" id="" />
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang=""></style>
