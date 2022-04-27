37d5b9a9a6bf847739dd04f1e55476e0f854b1d4
Habibur-Rahman0927 <habibur.rahaman.0927@gmail.com>
2022-04-27 13:07:05 +0600

    10_vue_project working with vue cli and components and props

---
+++ 10_vue_project_cli/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <FriendComponent
+    name="Rockstar"
+    phone="01221"
+    email="haibn"
+    is_fevorite="1"
+    friends
+  />
+  <!-- <friend-contact /> -->
+</template>
+
+<script>
+import FriendComponent from "./components/HelloWorld.vue";
+export default {
+  name: "App",
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenx",
+          phone: "0298948930",
+        },
+        {
+          id: "manuel",
+          name: "Manuel Lorenx",
+          phone: "0298948930",
+        },
+      ],
+    };
+  },
+  components: {
+    FriendComponent,
+  },
+};
+</script>
+
+<style></style>
---
+++ 10_vue_project_cli/src/components/HelloWorld.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <li>
+      <h2>
+        {{ friend.name }} {{ friendIsFevorite === "1" ? "(Fevorite)" : "" }}
+      </h2>
+      <button @click="toggleFavorite">Toggle Favorite</button>
+      <button @click="toggleDetails">Show Details</button>
+      <ul v-if="detailsAreVisible">
+        <li>
+          <strong>Phone: </strong>
+          {{ phone }}
+        </li>
+        <li><strong>email: </strong> {{ email }}</li>
+
+        <li><strong>name: </strong>{{ name }}</li>
+      </ul>
+    </li>
+  </div>
+</template>
+<script>
+export default {
+  // props: ["name", "phone", "email", "is_fevorite"],
+  props: {
+    name: String,
+    phone: String,
+    email: String,
+    is_fevorite: String,
+  },
+  name: "HelloWorld",
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "Hello",
+        name: "Hello Wolrd",
+        phone: "014 52265 22",
+        email: "habibur.rahmna.0927@gmail.com",
+      },
+      friendIsFevorite: this.is_fevorite,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      if (this.friendIsFevorite === "1") {
+        this.friendIsFevorite = "0";
+      } else {
+        this.friendIsFevorite = "1";
+      }
+    },
+  },
+};
+</script>
+<style></style>
