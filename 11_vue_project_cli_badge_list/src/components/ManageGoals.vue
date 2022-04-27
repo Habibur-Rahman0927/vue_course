424ddc3b3a2ed257b1f2364960e949263e720869
Habibur-Rahman0927 <habibur.rahaman.0927@gmail.com>
2022-04-28 00:15:43 +0600

    11_vue_project_cli_badge_list component render and teleport assing in  body tag

--- 11_vue_project_cli_badge_list/src/components/ManageGoals.vue
+++ 11_vue_project_cli_badge_list/src/components/ManageGoals.vue
@@ -1,10 +1,40 @@
 <template>
   <div>
     <h2>Manage Goals</h2>
-    <input type="text" name="" id="" />
+    <input type="text" ref="goal" name="" id="" />
+    <button @click="setGoal">Set Goal</button>
+    <teleport to="body">
+      <ErrorAlert v-if="inputIsInvalid">
+        <h2>Input is invalid!</h2>
+        <p>Please enter at laest a few characters...</p>
+        <button @click="confirmError">close</button>
+      </ErrorAlert>
+    </teleport>
   </div>
 </template>
 <script>
-export default {};
+import ErrorAlert from "./ErrorAlert.vue";
+export default {
+  comments: {
+    ErrorAlert,
+  },
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+      if (enteredValue === "") {
+        // alert("Input must not be empty");
+        this.inputIsInvalid = true;
+      }
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
+    },
+  },
+};
 </script>
 <style lang=""></style>
