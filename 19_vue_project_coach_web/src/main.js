f94e400c1d39a5faa7efb2e1259e84c30d7816a4
Habibur-Rahman0927 <habibur.rahaman.0927@gmail.com>
2022-04-30 02:14:28 +0600

    19_vue_project_coach_web working runing

--- 19_vue_project_coach_web/src/main.js
+++ 19_vue_project_coach_web/src/main.js
@@ -2,5 +2,12 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import BaseCard from "./components/ui/BaseCard.vue";
+import BaseButton from "./components/ui/BaseButton.vue";
 
-createApp(App).use(store).use(router).mount("#app");
+const app = createApp(App);
+app.component(BaseCard);
+app.component(BaseButton);
+app.use(router);
+app.use(store);
+app.mount("#app");
--- 19_vue_project_coach_web/src/pages/coaches/CoachesList.vue
+++ 19_vue_project_coach_web/src/pages/coaches/CoachesList.vue
@@ -1,20 +1,32 @@
 <template>
   <section>FILTER</section>
   <section>
-    <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register">Register As Coach</router-link>
-    </div>
-    <ul v-if="hasCoaches">
-      <li v-for="coach in filteredCoaches" :key="coach.id">
-        {{ coach.firstName }}
-      </li>
-    </ul>
-    <h3 v-else>No Coachs found</h3>
+    <base-card>
+      <div class="controls">
+        <base-button mode="outline">Refresh</base-button>
+        <base-button link to="/register">Register As Coach</base-button>
+      </div>
+      <ul v-if="hasCoaches">
+        <CoachItem
+          v-for="coach in filteredCoaches"
+          :key="coach.id"
+          :id="coach.id"
+          :first-name="coach.firstName"
+          :last-name="coach.lastName"
+          :rate="coach.hourlyRate"
+          :areas="coach.areas"
+        />
+      </ul>
+      <h3 v-else>No Coachs found</h3>
+    </base-card>
   </section>
 </template>
 <script>
+import CoachItem from "../../components/coaches/CoachItem.vue";
 export default {
+  components: {
+    CoachItem,
+  },
   computed: {
     filteredCoaches() {
       return this.$store.getters["coaches/coaches"];
@@ -25,4 +37,15 @@ export default {
   },
 };
 </script>
-<style></style>
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
