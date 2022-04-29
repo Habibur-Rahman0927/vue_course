d4ab402c69070b444534d973f2e08cc987df92e2
Habibur-Rahman0927 <habibur.rahaman.0927@gmail.com>
2022-04-29 20:42:08 +0600

    19_vue_project_coach_web creating some page and setting routes

---
+++ 19_vue_project_coach_web/src/router/index.js
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from "vue-router";
+import CoachDetail from "../pages/coaches/CoachesDetails.vue";
+import CoachesList from "../pages/coaches/CoachesList.vue";
+import CoachRegistration from "../pages/coaches/CoachRegistration.vue";
+import ContactCoach from "../pages/requests/ContactCoach.vue";
+import RequestReceive from "../pages/requests/RequestReceive.vue";
+import NotFound from "../pages/NotFound.vue";
+
+const routes = [
+  { path: "/", component: "/coaches" },
+  { path: "/coaches", component: CoachesList },
+  {
+    path: "/coaches/:id",
+    component: CoachDetail,
+    children: [{ path: "contact", component: ContactCoach }],
+  },
+  { path: "/register", component: CoachRegistration },
+  { path: "/requests", component: RequestReceive },
+  { path: "/:notFound(.*)", component: NotFound },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
