8cc98d859a2808169b51bb8e215cffabe8e83be8
Habibur-Rahman0927 <habibur.rahaman.0927@gmail.com>
2022-04-29 22:05:28 +0600

    19_vue_project_coach_web vuex state coaches modules create some data and CoachList view this data

--- 19_vue_project_coach_web/src/store/index.js
+++ 19_vue_project_coach_web/src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore } from "vuex";
-
+import coachModules from "./modules/coaches/index.js";
 export default createStore({
+  modules: {
+    coaches: coachModules,
+  },
   state: {},
   getters: {},
   mutations: {},
   actions: {},
-  modules: {},
 });
