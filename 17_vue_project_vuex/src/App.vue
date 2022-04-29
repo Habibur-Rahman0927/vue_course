746626c89c5d451f153d8d43ef3eda118bb53803
Habibur-Rahman0927 <habibur.rahaman.0927@gmail.com>
2022-04-29 10:31:35 +0600

    17_vue_project working with mutation action state getters etc

--- 17_vue_project_vuex/src/App.vue
+++ 17_vue_project_vuex/src/App.vue
@@ -1,16 +1,36 @@
 <template>
   <base-container title="Vuex">
-    <h3>{{ $store.state.counter }}</h3>
-    <button>add-1</button>
+    <h3>{{ counter }}</h3>
+    <NewCounter />
+    <button type="button" @click="addOne">add 2</button>
   </base-container>
 </template>
 
 <script>
 import BaseContainer from "./components/BaseContainer.vue";
+import NewCounter from "./components/NewCounter.vue";
 
 export default {
   components: {
     BaseContainer,
+    NewCounter,
+  },
+  computed: {
+    counter() {
+      return this.$store.state.counter;
+    },
+  },
+  methods: {
+    addOne() {
+      console.log("click");
+      // this.$store.state.counter++;
+      // this.$store.commit("increment", 2);
+      // this.$store.commit({
+      //   type: "increment",
+      //   value: 1,
+      // });
+      this.$store.dispatch("increment", 2);
+    },
   },
 };
 </script>
--- 17_vue_project_vuex/src/main.js
+++ 17_vue_project_vuex/src/main.js
@@ -8,6 +8,33 @@ const store = createStore({
       counter: 0,
     };
   },
+  mutations: {
+    increment(state, payload) {
+      state.counter = state.counter + payload;
+    },
+  },
+  actions: {
+    increment(context, payload) {
+      setTimeout(() => {
+        context.commit("increment", payload);
+      }, 2000);
+    },
+  },
+  getters: {
+    finalCounter(state) {
+      return state.counter * 5;
+    },
+    normalizedCounter(state, getters) {
+      const finalCounter = getters.finalCounter;
+      if (finalCounter < 0) {
+        return 0;
+      }
+      if (finalCounter > 100) {
+        return 100;
+      }
+      return finalCounter;
+    },
+  },
 });
 
 const app = createApp(App);
